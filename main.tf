6d6180995cad1f9f0b838dd1e5793d66f6ee4b25
lalita2407 <lalitalallu02@gmail.com>
2024-10-04 16:02:29 +0530

    skip_provider_registration

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "azurerm_linux_function_app" "example" {
     vnet_route_all_enabled = var.vnet_route_all_enabled
   }
 
-  depends_on = [ azurerm_service_plan.app_service_plan ]
+  depends_on = [ azurerm_app_service_plan.app_service_plan ]
 
   
 }
--- output.tf
+++
@@ -1,4 +0,0 @@
-output "function_app" {
-  description = "The ID of the Function App."
-  value       = azurerm_function_app.function_app.default_hostname
-}
